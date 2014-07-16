b5ac9e9b83b4b176a42a1d94a1376a537835de0f
XiongJian <420048248@qq.com>
2014-07-16 17:29:22 +0800

    Initial Commit

---
+++ Demo2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo2
+//
+//  Created by XiongJian on 14-7-16.
+//  Copyright (c) 2014年 Static. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Demo2
+//
+//  Created by XiongJian on 14-7-16.
+//  Copyright (c) 2014年 Static. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
